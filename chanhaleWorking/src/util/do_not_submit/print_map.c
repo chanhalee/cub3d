69562845227e887b81885e1c0afdf5c53c45e8be
chanhale <chan771788@gmail.com>
2022-08-10 02:36:45 +0900

    complex testData and print map enhanced

--- chanhaleWorking/src/util/do_not_submit/print_map.c
+++ chanhaleWorking/src/util/do_not_submit/print_map.c
@@ -4,11 +4,13 @@ void	print_map(t_map *map)
 {
 	int		counter;
 
-	counter = -1;
+	counter = map->size_y;
 	printf("\n==========print_map==========\n");
-	while(++counter < map->size_y)
+	while(--counter >= 0)
 	{
 		printf("[%d] %s\n", counter, map->map[counter]);
 	}
+	printf("p_pos: %lf, %lf\n", map->player.pos_x,  map->player.pos_x);
+	printf("p_theta: %lf\n", map->player.vision_theta / TYPE_PI * 180);
 	printf("=============================\n");
 }
