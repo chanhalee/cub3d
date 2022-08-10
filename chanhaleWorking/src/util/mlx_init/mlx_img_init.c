0e081e7b004c5c53fd173a8710db37d29fadbe0b
chanhale <chan771788@gmail.com>
2022-08-10 14:29:16 +0900

    mlx_img_init added

--- chanhaleWorking/src/util/mlx_init/mlx_img_init.c
+++ chanhaleWorking/src/util/mlx_init/mlx_img_init.c
@@ -6,16 +6,35 @@
 /*   By: chanhale <chanhale@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 20:11:54 by chanhale          #+#    #+#             */
-/*   Updated: 2022/08/10 14:12:02 by chanhale         ###   ########.fr       */
+/*   Updated: 2022/08/10 14:24:29 by chanhale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/RayCasting.h"
 
-void	 mlx_img_init(t_mlx *mlx)
+int	mlx_img_init(t_mlx *mlx)
 {
 	char	*temp;
+	int	img_w;
+	int	img_h;
 
 	temp = (char *)mlx->e_img;
-	mlx->e_img = mlx_xpm_file_to_image()
+	mlx->e_img = mlx_xpm_file_to_image(mlx->mlx, temp, &img_w, &img_h);
+	if (temp != NULL)
+		free (temp);
+	temp = (char *)mlx->w_img;
+	mlx->w_img = mlx_xpm_file_to_image(mlx->mlx, temp, &img_w, &img_h);
+	if (temp != NULL)
+		free (temp);
+	temp = (char *)mlx->s_img;
+	mlx->s_img = mlx_xpm_file_to_image(mlx->mlx, temp, &img_w, &img_h);
+	if (temp != NULL)
+		free (temp);
+	temp = (char *)mlx->n_img;
+	mlx->n_img = mlx_xpm_file_to_image(mlx->mlx, temp, &img_w, &img_h);
+	if (temp != NULL)
+		free (temp);
+	if (mlx->e_img && mlx->w_img && mlx->s_img && mlx->n_img)
+		return (0);
+	return (1);
 }
