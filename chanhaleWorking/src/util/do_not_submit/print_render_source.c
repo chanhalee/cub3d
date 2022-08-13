c3f2683ac896f1c3762f6cbad05c737e61a1c1ac
chanhale <chan771788@gmail.com>
2022-08-13 14:00:56 +0900

    chanhaleWorking dir minor fix

--- chanhaleWorking/src/util/do_not_submit/print_render_source.c
+++ chanhaleWorking/src/util/do_not_submit/print_render_source.c
@@ -8,10 +8,7 @@ void	print_render_source(t_render_source *r_s)
 	while (++counter < TYPE_HOR_PIX)
 	{
 		printf("[%d]dis: %lf, ", counter, r_s[counter].distance);
-		printf("x: %lf, ",r_s[counter].ob_x);
-		printf("y: %lf, ",r_s[counter].ob_y);
 		printf("o_p: %d, ",r_s[counter].object_pos);
-		printf("the: %lf, ",r_s[counter].theta);
 		printf("w_p: %d\n",r_s[counter].render_screen_pos);
 	}
 }
